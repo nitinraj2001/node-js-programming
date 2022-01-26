0194fe26dc85c99d26753cf311c7d2eb0dce3c1a
nitinraj2001 <nitinraj2001@gmail.com>
2022-01-26 22:55:39 +0530

    cretaed myfirstmodule and include it in js file and learn how to split query in request

--- Programs/hello-world.js
+++ Programs/hello-world.js
@@ -1,5 +1,9 @@
-var http=require('http')
+var http=require('http');
+const { url } = require('inspector');
+var dt=require('./myFirstModule');
 http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type':'text/html'});
-    res.end('hello world'+'Welcome to the world of programming');
+    res.write(req.url);
+    res.write("Current data and time in system is: "+dt.currentDateAndTime());
+    res.end('hello world '+'Welcome to the world of programming');
 }).listen(9000);
